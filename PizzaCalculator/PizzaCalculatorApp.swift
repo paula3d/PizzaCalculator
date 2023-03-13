42834004a3389bb57d44f7f0b40e90b5855dc0af
Paulina Dąbrowska <paulina@macbook-pro-paulina.home>
2023-03-13 12:59:53 +0100

    Adding initial files

---
+++ PizzaCalculator/PizzaCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaCalculatorApp.swift
+//  PizzaCalculator
+//
+//  Created by Paulina Dąbrowska on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PizzaCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
